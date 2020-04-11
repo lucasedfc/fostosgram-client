f42c14013b4f1c3dd28ec3caae65350e23588199
Lucas Heim <lucasedfc@gmail.com>
2020-04-11 12:49:43 -0300

    Login Page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  avatars = [
+    {
+      img: 'av-1.png',
+      selected: true
+    },
+    {
+      img: 'av-2.png',
+      selected: false
+    },
+    {
+      img: 'av-3.png',
+      selected: false
+    },
+    {
+      img: 'av-4.png',
+      selected: false
+    },
+    {
+      img: 'av-5.png',
+      selected: false
+    },
+    {
+      img: 'av-6.png',
+      selected: false
+    },
+    {
+      img: 'av-7.png',
+      selected: false
+    },
+    {
+      img: 'av-8.png',
+      selected: false
+    },
+];
+
+avatarSlide = {
+    slidesPerView: 3.5
+};
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(fLogin: NgForm) {
+    console.log(fLogin.valid);
+  }
+
+  register(fRegister: NgForm) {
+    console.log(fRegister.valid);
+  }
+
+  selectAvatar(avatar) {
+    this.avatars.forEach(av => av.selected = false);
+    avatar.selected = true;
   }
 
 }
